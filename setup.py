dd1bc8963a2d92efc78132fb1cbc083cefee1951
Ivan Stanojevic <ivan@lightspeedretail.com>
2017-10-06 15:21:37 -0700

    Setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="lightspeed_sdk",
+      version="0.1",
+      description="Python SDK for lightspeed retail API",
+      author="Ivan Stanojevic",
+      author_email="ivanst@gmail.com",
+      packages=["lightspeed_sdk"]
+      )
